1f6ea3d426e20eebb54a85ec5a87b39635f9016a
SandraCynado <Casanadis@gmail.com>
2022-10-05 06:16:24 -0700

    ./args You can do anything, but not everything.

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main- print the name of the program
+ * @argc: count arguements
+ * @argv: arguments
+ *
+ * Return: always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+       /*declaring variables*/
+	int count = 0;
+
+	if (argc > 0)
+	{
+		/*WHILE - print each arguments*/
+		while (count < argc)
+		{
+			printf("%s\n", argv[count]);
+			count++;
+		}
+	}
+	return (0);
+}
